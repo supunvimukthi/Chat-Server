47484a6610332fa08fddc62e3a1eb6322f27281c
Supun Ranwaka <supun.15@cse.mrt.ac.lk>
2018-07-06 16:25:18 +0530

    make it better

---
+++ routes/messages.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/Chat');
+mongoose.Promise = global.Promise;
+var Schema = mongoose.Schema;
+
+var messageDataSchema = new Schema({
+    message:String,
+    sender:String,
+    receiver:String,
+    time:String,
+    date:String
+}, {collection: 'chat'});
+
+var MessageData = mongoose.model('MessageData', messageDataSchema);
+
+//save sent messages in the database
+router.post('/send', function (req, res, next) {
+    
+    var message = req.body.message;
+    var sender = req.body.sender;
+    var receiver = req.body.receiver;
+    var time = req.body.time;
+    var date = req.body.date;
+
+    console.log(message, sender, receiver, time, date);
+    var item= { 
+        message: message, 
+        sender: sender, 
+        receiver: receiver,
+        time: time, 
+        date: date
+     }
+    var data = new MessageData(item);
+    data.save();
+    res.send({ 'message': 'message sent' });
+});
+
+
+//update message list
+router.post('/receive', function (req, res, next) {
+    
+    var sender = req.body.sender;
+    var receiver = req.body.receiver;
+    console.log(sender, receiver);
+
+    MessageData.find({ $or: [{ sender: sender, receiver: receiver }, { sender: receiver, receiver: sender }] }).sort({ "date": 1 })
+    .then(function(items) {
+    res.send({'data': items});
+    })
+    .catch(function(err) {
+    console.log(err);
+    }); 
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/Chat');
+mongoose.Promise = global.Promise;
+var Schema = mongoose.Schema;
+
+var userDataSchema = new Schema({
+  first:String,
+  last: String,
+  email: String,
+  thumbnail:String,
+  password:String
+}, {collection: 'users'});
+
+var UserData = mongoose.model('users', userDataSchema);
+
+//generate a list of all contacts except the logged in user
+router.get('/list/:user', function(req, res,next) {
+  var user=req.params.user; 
+  console.log(user);
+  UserData.find({ 'email':{$ne : user} })
+  .then(function(items) {
+    res.send({'results': items});
+  })
+  .catch(function(err) {
+    console.log(err);
+  }); 
+});
+
+//check for login credentials
+router.post('/login', function(req, res, next) {
+  
+  var username=req.body.username;
+  var password=req.body.password;
+
+  UserData.find({ email: username, password: password})
+  .then(function(items) {
+    if(items.length===1){
+      res.send({
+        'success':true,
+        'user':username,
+    });
+    }
+    else{
+      res.send({
+        'message':'Wrong login credentials'
+      });
+    } 
+  })
+  .catch(function(err) {
+    console.log(err);
+  });  
+});
+
+module.exports = router;
